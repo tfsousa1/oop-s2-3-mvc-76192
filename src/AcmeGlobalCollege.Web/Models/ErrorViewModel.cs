6dc80c63ceea3e1a0a12e4861cea847a2c06803f
tfsousa1 <tamires.fs@gmail.com>
2026-04-05 09:39:43 +0100

    Initial solution structure (MVC + Tests)

---
+++ src/AcmeGlobalCollege.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AcmeGlobalCollege.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
