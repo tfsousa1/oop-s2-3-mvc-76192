485f5645550e44680935839b2bf369c3767a22e1
tfsousa1 <tamires.fs@gmail.com>
2026-04-06 18:56:33 +0100

    Add admin student listing and extend role-based navigation

---
+++ src/AcmeGlobalCollege.Web/Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using AcmeGlobalCollege.Web.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcmeGlobalCollege.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var students = await _context.StudentProfiles
+                .OrderBy(s => s.LastName)
+                .ThenBy(s => s.FirstName)
+                .ToListAsync();
+
+            return View(students);
+        }
+    }
+}
